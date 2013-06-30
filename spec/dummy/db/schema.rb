c063c57cdd47d8259b131352599d73361d29323c
Arthur Shagall <arthur.shagall@gmail.com>
2013-06-30 18:09:50 -0500

    Polishing up code for 2.0.0.rc release.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -9,69 +9,69 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120909235526) do
+ActiveRecord::Schema.define(version: 20120909235526) do
 
-  create_table "adapters", :force => true do |t|
+  create_table "adapters", force: true do |t|
     t.integer  "connector_type_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "booking_statuses", :force => true do |t|
+  create_table "booking_statuses", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "bookings", :force => true do |t|
+  create_table "bookings", force: true do |t|
     t.integer  "status_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "state_id"
   end
 
-  create_table "colors", :force => true do |t|
-    t.string "name", :null => false
+  create_table "colors", force: true do |t|
+    t.string "name", null: false
   end
 
-  add_index "colors", ["name"], :name => "index_colors_on_name", :unique => true
+  add_index "colors", ["name"], name: "index_colors_on_name", unique: true
 
-  create_table "connector_types", :force => true do |t|
-    t.string   "name",        :limit => 50,                   :null => false
+  create_table "connector_types", force: true do |t|
+    t.string   "name",        limit: 50,                null: false
     t.string   "description"
-    t.boolean  "active",                    :default => true, :null => false
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
-    t.boolean  "has_sound",                                   :null => false
+    t.boolean  "active",                 default: true, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "has_sound",                             null: false
   end
 
-  add_index "connector_types", ["name"], :name => "index_connector_types_on_name", :unique => true
+  add_index "connector_types", ["name"], name: "index_connector_types_on_name", unique: true
 
-  create_table "fruits", :force => true do |t|
-    t.string "fruit_name",  :null => false
+  create_table "fruits", force: true do |t|
+    t.string "fruit_name",  null: false
     t.string "description"
   end
 
-  add_index "fruits", ["fruit_name"], :name => "index_fruits_on_fruit_name", :unique => true
+  add_index "fruits", ["fruit_name"], name: "index_fruits_on_fruit_name", unique: true
 
-  create_table "states", :force => true do |t|
+  create_table "states", force: true do |t|
     t.string   "state_code"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "virtual_enums", :force => true do |t|
-    t.string "name", :null => false
+  create_table "virtual_enums", force: true do |t|
+    t.string "name", null: false
   end
 
-  add_index "virtual_enums", ["name"], :name => "index_virtual_enums_on_name", :unique => true
+  add_index "virtual_enums", ["name"], name: "index_virtual_enums_on_name", unique: true
 
-  create_table "widgets", :force => true do |t|
+  create_table "widgets", force: true do |t|
     t.integer  "connector_type_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
