59d29939f3066935f14ecdf1dbe0a8d993c9da3a
Zachary Belzer <zbelzer@gmail.com>
2014-01-20 15:46:42 -0600

    Follow API for migration number methods.
    The original methods are established in
    railties/lib/rails/generators/migration.rb

--- lib/generators/enum/enum_generator_helpers/migration_number.rb
+++ lib/generators/enum/enum_generator_helpers/migration_number.rb
@@ -5,8 +5,8 @@ module EnumGeneratorHelpers
 
     # Returns the number of the last migration.
     # @return [Fixnum]
-    def current_migration_number
-      dirname = "#{Rails.root}/db/migrate/[0-9]*_*.rb"
+    def current_migration_number(dirname = nil)
+      dirname ||= "#{Rails.root}/db/migrate/[0-9]*_*.rb"
       Dir.glob(dirname).collect do |file|
         File.basename(file).split("_").first.to_i
       end.max.to_i
@@ -15,10 +15,10 @@ module EnumGeneratorHelpers
     # Returns the next upcoming migration number.  Sadly, Rails has no API for
     # this, so we're reduced to copying from ActiveRecord::Generators::Migration
     # @return [Fixnum]
-    def next_migration_number
+    def next_migration_number(dirname = nil)
       # Lifted directly from ActiveRecord::Generators::Migration
       # Unfortunately, no API is provided by Rails at this time.
-      next_migration_number = current_migration_number + 1
+      next_migration_number = current_migration_number(dirname) + 1
       if ActiveRecord::Base.timestamped_migrations
         [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
       else
